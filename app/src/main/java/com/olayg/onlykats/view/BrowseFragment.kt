c250b0bc3bc930bb3f57b07ff65b1f5a230eb349
olayg <ogureje@yahoo.com>
2021-09-03 11:48:47 -0400

    Setting up navigation component

--- app/src/main/java/com/olayg/onlykats/view/BrowseFragment.kt
+++ app/src/main/java/com/olayg/onlykats/view/BrowseFragment.kt
@@ -2,7 +2,9 @@ package com.olayg.onlykats.view
 
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.olayg.onlykats.R
 import com.olayg.onlykats.databinding.FragmentBrowseBinding
 
@@ -16,6 +18,23 @@ class BrowseFragment : Fragment(R.layout.fragment_browse) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentBrowseBinding.bind(view)
+        initViews()
+    }
+
+    // with(receiver) is 1 of 5 scope functions
+    private fun initViews() = with(binding) {
+        btnDetails.setOnClickListener {
+            /*
+            If we arent using safeArgs we can directly call the action id
+            findNavController().navigate(R.id.action_detailsFragment)
+            */
+            val direction = BrowseFragmentDirections.actionDetailsFragment()
+            findNavController().navigate(direction)
+
+        }
+        btnSettings.setOnClickListener {
+            findNavController().navigate(R.id.action_settingsFragment)
+        }
     }
 
 }
