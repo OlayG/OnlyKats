6ad61b52277681a3ee403a8021a49c0e0672c6bc
joseph <josephdanielmcfatridge@gmail.com>
2021-09-13 15:10:39 -0400

    Set up the breedAdapter and it will now show breeds

--- app/src/main/java/com/olayg/onlykats/view/BrowseFragment.kt
+++ app/src/main/java/com/olayg/onlykats/view/BrowseFragment.kt
@@ -13,6 +13,7 @@ import androidx.recyclerview.widget.RecyclerView
 import com.olayg.onlykats.adapter.BreedAdapter
 import com.olayg.onlykats.adapter.KatAdapter
 import com.olayg.onlykats.databinding.FragmentBrowseBinding
+import com.olayg.onlykats.model.Breed
 import com.olayg.onlykats.model.Kat
 import com.olayg.onlykats.util.ApiState
 import com.olayg.onlykats.util.PageAction
@@ -38,6 +39,7 @@ class BrowseFragment : Fragment() {
         savedInstanceState: Bundle?
     ) = FragmentBrowseBinding.inflate(layoutInflater, container, false).also {
         _binding = it
+        initViews()
     }.root
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -71,6 +73,11 @@ class BrowseFragment : Fragment() {
             if (state is ApiState.Success) loadKats(state.data)
             if (state is ApiState.Failure) handleFailure(state.errorMsg)
         }
+        breedState.observe(viewLifecycleOwner) { state ->
+            binding.pbLoading.isVisible = state is ApiState.Loading
+            if (state is ApiState.Success) loadBreeds(state.data)
+            if (state is ApiState.Failure) handleFailure(state.errorMsg)
+        }
     }
 
     private fun loadKats(kats: List<Kat>) = with(binding.rvList) {
@@ -78,6 +85,13 @@ class BrowseFragment : Fragment() {
         if (adapter == null) adapter = katAdapter
         breedAdapter.clear()
         katAdapter.updateList(kats)
+    }
+
+    private fun loadBreeds(breeds: List<Breed>) = with(binding.rvList) {
+        Log.d(TAG, "ApiState.Success: $breeds")
+        if (adapter == null) adapter = breedAdapter
+        breedAdapter.clear()
+        breedAdapter.updateList(breeds)
     }
 
     private fun handleFailure(errorMsg: String) {
